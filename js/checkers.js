93552544203502427237ad8bead761a915dc94e6
foozie3moons <matthew.bell.290@gmail.com>
2017-09-11 09:59:21 -0700

    gamepiece functionality added; revert on invalid move functional

--- js/checkers.js
+++ js/checkers.js
@@ -10,9 +10,19 @@ $(document).ready(function() {
     buildHeader();
     buildMain();
     buildFooter();
+    $('footer').append("<div>", {class: "gamepiece black-king"});
+
   });
 
   function generateBoard(player, cols, rows) {
+
+    var getLetters = function(cols, reverse) {
+      if (reverse) {
+        return 'abcdefghijklmnopqrstuvwxyz'.split('').slice(0,cols).reverse();
+      } else {
+        return 'abcdefghijklmnopqrstuvwxyz'.split('').slice(0,cols);
+      }
+    }
 
     var $main = $("main");
     var $table = $("<table>", {class: "gameboard " + player});
@@ -23,27 +33,36 @@ $(document).ready(function() {
 
     $main.append($table);
     $table.append($caption);
-    $caption.append($("<h2>" + player + "'s board</h2>"));
+    $caption.append($("<h2>" + player.toUpperCase() + "'S BOARD</h2>"));
     $table.append($thead).append($tbody);
     $thead.append($theadtr);
     $tbody.append($tbodytr);
 
-    // head X row
-    for (var i = 0; i < cols + 1; i++) {
-      var letters = 'abcdefghijklmnopqrstuvwxyz'.split('').slice(0,cols)
-      var $theadth = $("<th>", {class: "thead-th"});
-      $theadtr.append($theadth);
-      if (player === "black") {
-        letters = letters.reverse();
+    if (player === "black") {
+      for (var i = 0; i < cols + 1; i++) {
+        var $theadth = $("<th>", {class: "thead-th"});
+        $theadtr.append($theadth);
+        var letters = getLetters(cols, true);
+        if (i !== 0) {
+          $theadth.text(letters[i-1]);
+        }
       }
-      if (i !== 0) {
-        $theadth.text(letters[i-1]);
+    } else if (player === "red") {
+      for (var i = 0; i < cols + 1; i++) {
+        var $theadth = $("<th>", {scope: "col", class: "thead-th"});
+        $theadtr.append($theadth);
+        var letters = getLetters(cols, false);
+        if (i !== 0) {
+          $theadth.text(letters[i-1]);
+        }
       }
     }
 
+    // head X row
+
     for (var i = 0; i < rows; i++) {
       var $tbodytr = $("<tr>", {class: "tbody-tr"});
-      var $tbodyth = $("<th>", {class: "tbody-th"});
+      var $tbodyth = $("<th>", {scope: "row", class: "tbody-th"});
       $tbody.append($tbodytr);
       $tbodytr.append($tbodyth);
       if (player === "red") {
@@ -52,25 +71,73 @@ $(document).ready(function() {
       } else {
         $tbodyth.text(i+1);
       }
-      for (var j = 0; j < cols; j++) {
+      for (var j = 1; j < cols+1; j++) {
+        var letters = 'abcdefghijklmnopqrstuvwxyz'.split('').slice(0,cols)
         var $tbodytd = $("<td>", {class: "tbody-td"});
         $tbodytr.append($tbodytd);
+        if (player === "black") {
+          // reverses col for player black
+          $tbodytd.attr("data-col", letters[Math.abs(j - cols)]);
+        } else {
+          $tbodytd.attr("data-col", letters[j]);
+        }
+        $tbodytd.attr("data-row", i+1);
       }
     }
   }
 
-  function addPieces() {
+  function addPieces(player) {
+    // if black:
+    // red rows = 1, 2, 3
+    // black rows = 6, 7, 8
+
+
     var $table = $("<table>", {class: "gameboard " + player});
     var $trs = $table.find('tr');
     for (let i = 0; i < $trs.length; i++) {
       $table.find('tr').each(function (i, el) {
         var $tds = $(this).find('td');
-
       });
     }
+    $trs.each
       var $tds = $(this).find('td');
-      $td.data("col", i);
-      // do something with productId, product, Quantity
+      //$td.data("col", i);
+  }
+
+  function dragAndDrop() {
+    var $tdValid = $(".tbody-tr:nth-child(even) td:nth-child(odd), .tbody-tr:nth-child(odd) td:nth-child(even)");
+    var $tdInvalid = $(".tbody-tr:nth-child(odd) td:nth-child(odd), .tbody-tr:nth-child(even) td:nth-child(even)");
+    var outside = false;
+    $("[data-col=h][data-row=1]").wrapInner("<div class='gamepiece black-pawn'>");
+
+    $(".gamepiece").draggable({
+      revert: "invalid",
+      cursor: "move"
+    });
+
+    $('tdbody-td').droppable();
+
+    $(".gamepiece").droppable({});
+
+    $tdValid.droppable({
+      accept: ".gamepiece",
+      drop: function(event, ui) {
+
+        var $this = $(this);
+        $this.append(ui.draggable);
+
+        var width = $this.width();
+        var height = $this.height();
+
+        var cntrLeft = width / 2 - ui.draggable.width() / 2;// + left;
+        var cntrTop = height / 2 - ui.draggable.height() / 2;// + top;
+
+        ui.draggable.css({
+            left: cntrLeft + "px",
+            top: cntrTop + "px"
+        });
+      },
+    });
   }
 
   function buildHeader() {
@@ -83,13 +150,14 @@ $(document).ready(function() {
     var cols = 8;
     var rows = 8;
     generateBoard("black", cols, rows);
-    generateBoard("red", cols, rows);
-    addPieces();
+    // generateBoard("red", cols, rows);
+    dragAndDrop();
+    //addPieces("black");
   }
   function buildFooter() {
     $footer = $("footer");
     $h4 = $("<h4>");
-    $h4.html("<sup>&copy;</sup>" + " Matthew Bell");
+    $h4.html("&copy;" + " Matthew Bell aka Foozie3Moons");
     $footer.append($h4);
   }
 
